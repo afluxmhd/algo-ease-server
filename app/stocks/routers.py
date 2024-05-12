cfaf1f6dc6ec63c19a99f397e2a63abde6059a7e
Aflah <93920274+afluxmhd@users.noreply.github.com>
2024-05-12 23:03:52 +0530

    Fix var name

--- app/stocks/routers.py
+++ app/stocks/routers.py
@@ -9,12 +9,9 @@ router = APIRouter()
 
 @router.post("/stock-price",response_model=StockRes,include_in_schema=False)
 async def get_current_price(stock : StockReq):
-    # current_price = round(uniform(400, 500), 2)
-    # current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    # stock = Stock(name=name,symbol='NOT_DEFINED',price=current_price,time=current_time)
-    # Instanse of stock simulation
-    stockSimulation = stock_simulation()
-    res = stockSimulation.stock_price(stock.status, stock.entry, stock.exit, stock.action)
+
+    stock_simulation = StockSimulation()
+    res = stock_simulation.stock_price(stock.status, stock.entry, stock.exit, stock.action)
     print(stock.status,stock.entry,stock.exit,stock.action,2)
 
     stockres = StockRes(
